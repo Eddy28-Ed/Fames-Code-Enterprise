d9e248641f690baf285a562303e99414260e8adc
Dawry13 <alexisencarnacionencarnac1305@gmail.com>
2026-02-06 01:50:54 -0400

    Add files via upload

---
+++ Asignacion 1/NumerosImpares.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.numerosimpares;
+
+/**
+ *
+ * @author DAWRY13
+ */
+public class NumerosImpares {
+
+    public static void main(String[] args) {
+        for (int i=1; i <= 100; i += 2){
+        System.out.println(i);
+    }
+        
+    }
+}
