74a8b764604797a29776e34cf4e70665645deda1
Dawry13 <alexisencarnacionencarnac1305@gmail.com>
2026-03-19 23:46:59 -0400

    Add files via upload

---
+++ Alexis Dawry Encarnacion 2024-2437/Tarea Práctica 4 - Java Swing/Calculadora.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadoragui;
+
+/**
+ *
+ * @author DAWRY13
+ */
+public class Calculadora {
+    public double sumar(Double valor1, Double valor2){
+        return valor1 + valor2;
+    }
+    
+    public double restar (Double valor1, Double valor2){
+        return valor1 - valor2;
+    }
+    
+    public double multiplicar (Double valor1, Double valor2){
+        return valor1 * valor2;
+    }
+    
+    public double dividir (Double valor1, Double valor2){
+        return valor1 / valor2;
+    }
+}
