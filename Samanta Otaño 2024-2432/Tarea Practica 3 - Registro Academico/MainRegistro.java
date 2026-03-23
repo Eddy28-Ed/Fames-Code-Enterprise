1977b575afcf2b18e2ab8dcd2b112cfb65b528a5
samii1121 <samantaotano@gmail.com>
2026-03-23 19:01:42 +0100

    Add files via upload

---
+++ Samanta Otaño 2024-2432/Tarea Practica 3 - Registro Academico/MainRegistro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Principal;
+
+/**
+ *
+ * @author User
+ */
+public class MainRegistro {
+    public static void main(String[] args) {
+        RegistroAcademicoGUI ventana = new RegistroAcademicoGUI();
+        ventana.setVisible(true);
+    }
+}
