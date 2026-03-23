c338a2bbd46298923ec61a87c1b729ff537c1c3a
samii1121 <samantaotano@gmail.com>
2026-03-23 19:52:49 +0100

    Add files via upload

---
+++ Samanta Otaño 2024-2432/ProcesarDatos.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Principal;
+
+/**
+ *
+ * @author User
+ */
+public class ProcesarDatos {
+
+    private String matricula;
+    private String nombres;
+    private String apellidos;
+    private String cuatrimestre;
+
+    public void capturarDatos(String matricula, String nombres,
+                              String apellidos, String cuatrimestre) {
+
+        this.matricula = matricula;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.cuatrimestre = cuatrimestre;
+    }
+
+    public String presentarDatos() {
+        return "Matrícula: " + matricula +
+               "\nNombres: " + nombres +
+               "\nApellidos: " + apellidos +
+               "\nCuatrimestre: " + cuatrimestre;
+    }
+}
